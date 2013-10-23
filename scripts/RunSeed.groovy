712d1b53506d804b6a195c23e480e67c8c73c729
Burt Beckwith <burt@burtbeckwith.com>
2013-10-22 20:57:21 -0400

    no need for reflection when using Groovy

--- scripts/RunSeed.groovy
+++ scripts/RunSeed.groovy
@@ -3,10 +3,7 @@ includeTargets << grailsScript("_GrailsBootstrap")
 target(runSeed: "Creates a seed file") {
 	depends(configureProxy,compile, packageApp, bootstrap)
 
-	def serviceClass = grailsApp.getClassForName("com.procon.nspire.seed.SeedService")
-	def serviceClassMethod = serviceClass.metaClass.getMetaMethod("installSeedData")
-	def service = appCtx.seedService
-	serviceClassMethod.invoke(service, null)
+	appCtx.seedService.installSeedData()
 }
 
 setDefaultTarget(runSeed)
