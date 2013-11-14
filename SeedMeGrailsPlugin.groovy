41f8fdb4a8df6e86e42876df7cd922210fdde412
bdwheeler <bdwheeler@gmail.com>
2013-11-14 11:35:08 -0800

    update to add a controller so you can hand install seed

--- SeedMeGrailsPlugin.groovy
+++ SeedMeGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SeedMeGrailsPlugin {
 
-	def version        = "0.2.0"
+	def version        = "0.2.1"
 	def grailsVersion  = "2.0 > *"
 	def pluginExcludes = [
 		"grails-app/domain/seedme/Child.groovy",
