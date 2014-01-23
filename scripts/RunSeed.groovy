e27f0652940e9b5b8f60986567c6e48659ba933b
A.J. Brown <aj@ajbrown.org>
2014-01-23 17:18:06 -0500

    Prevent occasional hibernate session errors during run-seed.
    In grails 2.3.5, I was running into the occasional "No Hibernate Session bound
    to thread, and configuration does not allow creation of non-transactional one
    here" exception when executing the run-seed task.  By forcing initialization of
    the persistenceInterceptor, we can prevent those exceptions.

--- scripts/RunSeed.groovy
+++ scripts/RunSeed.groovy
@@ -3,6 +3,9 @@ includeTargets << grailsScript("_GrailsBootstrap")
 target(runSeed: "Installs seed files") {
 	depends(configureProxy,compile, packageApp, bootstrap)
 
+    def persistenceInterceptor = appCtx.containsBean('persistenceInterceptor') ? appCtx.persistenceInterceptor : null
+    persistenceInterceptor?.init()
+
 	appCtx.seedService.installSeedData()
 }
 
