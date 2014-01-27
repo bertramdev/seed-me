aac4c6d1ef881076df01a0ac1ced7df1f747cc30
Bob Whiton <bwhiton@bertramcapital.com>
2014-01-27 08:10:12 -0700

    Inject grailApplication into seed files

--- SeedMeGrailsPlugin.groovy
+++ SeedMeGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SeedMeGrailsPlugin {
 
-	def version        = "0.4.3"
+	def version        = "0.4.4"
 	def grailsVersion  = "2.0 > *"
 	def pluginExcludes = [
 		"src/seed/*",
--- grails-app/services/seedme/SeedService.groovy
+++ grails-app/services/seedme/SeedService.groovy
@@ -80,6 +80,7 @@ class SeedService {
 		def rtn = [seedList:[], dependsOn:[], name:name, plugin:plugin]
 		try {
 			def tmpBinding = new Binding()
+			tmpBinding.setVariable("grailsApplication", grailsApplication)
 			def tmpConfig = new GroovyShell(tmpBinding).evaluate(seedContent)
 			rtn.checksum = DatatypeConverter.printBase64Binary(MessageDigest.getInstance('MD5').digest(seedContent.bytes))
 			def tmpBuilder = new SeedBuilder()
