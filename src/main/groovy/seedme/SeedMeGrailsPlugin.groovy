6e9687395ad92835515fac8fb04c59a1f93c4e05
Jeremy Michael Crosbie <jcrosbie@bertramcapital.com>
2019-05-20 12:23:44 -0600

    Remove usage of deprecated Grails Domain Class API
    See
    http://docs.grails.org/3.3.0/guide/upgrading.html#_grails_domain_class_api_deprecated
    
    Deprecated in 3.3.x this API has been removed entirely from Grails 4
    rendering SeedMe unworkable with that version of Grails.  These
    changes are compatible with Grails versions >= 3.3.0, including Grails
    4, so hopefully the version bump to 3.3.0 of this plugin is a helpful
    reminder.

--- src/main/groovy/seedme/SeedMeGrailsPlugin.groovy
+++ src/main/groovy/seedme/SeedMeGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class SeedMeGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.1.5 > *"
+    def grailsVersion = "3.3.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
