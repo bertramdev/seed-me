f133d9e34b8cbc6d66d2d80be288287353ce086d
bdwheeler <bdwheeler@gmail.com>
2021-08-12 15:27:28 -0600

    removing build

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '4.2.0-snapshot')
-        classNode.putNodeMetaData('projectName', 'seed-me')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
